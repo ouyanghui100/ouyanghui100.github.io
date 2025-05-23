28b78d74f62d4c8581dd4876f206dbf555c49b17
OuyangHui <2902897795@qq.com>
2025-05-23 14:29:51 +0800

    wip: 添加部分校验规则

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  },
+  settings: {
+    react: { version: 'detect' }
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createStyleImportPlugin } from 'vite-plugin-style-import';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-base: '/ouyanghui100.github.io/'
-})
+  plugins: [
+    react(),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: name => `antd/es/${name}/style/index`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@primary-color': '#1890ff', // 主题颜色配置
+        },
+      },
+    },
+  },
+  base: '/ouyanghui100.github.io/',
+});
