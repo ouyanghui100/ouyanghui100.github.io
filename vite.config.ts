bfbd31c5c7e5a3003bc507b90c9943109e73e889
OuyangHui <2902897795@qq.com>
2025-05-29 19:00:30 +0800

    wip: 尝试一些怪东西

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import * as reactPlugin from 'vite-plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // 解决报The requested module 'react-is/index.js' does not provide an export named 'ForwardRef'的问题
-  jsx: 'react',
-  // 解决报The requested module 'react-is/index.js' does not provide an export named 'ForwardRef'的问题
   optimizeDeps: {
     include: [
      'react-is',
